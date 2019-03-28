136639492d858eee6d44cc1a3049e71fef7e6d37
Vihaan <32998032+TravelerVihaan@users.noreply.github.com>
2019-03-28 11:53:20 +0100

    Add files via upload

---
+++ src/main/java/com/github/travelervihaan/clubmanagement/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.github.travelervihaan.clubmanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.github.travelervihaan.clubmanagement.model.Employee;
+import com.github.travelervihaan.clubmanagement.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	private EmployeeRepository employeeRepo;
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public EmployeeService(EmployeeRepository employeeRepo) {
+		this.employeeRepo = employeeRepo;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	public void addNewEmployee(Employee employee) {
+		// EmployeeRole defaultRole = roleRepository.findByRole(DEFAULT_ROLE);
+		String passwordHash = passwordEncoder.encode(employee.getPassword());
+		employee.setPassword(passwordHash);
+		employeeRepo.save(employee);
+	}
+}
