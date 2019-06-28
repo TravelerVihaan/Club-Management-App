64c75b8e810c57cfa09115588e17f0d223418c20
TravelerVihaan <lukasz.biel@10g.pl>
2019-06-28 22:20:55 +0200

    Absence Service

--- src/main/java/com/github/travelervihaan/clubmanagement/service/absences/AbsenceService.java
+++ src/main/java/com/github/travelervihaan/clubmanagement/service/absences/AbsenceService.java
@@ -3,19 +3,24 @@ package com.github.travelervihaan.clubmanagement.service.absences;
 import com.github.travelervihaan.clubmanagement.model.absences.Absence;
 import com.github.travelervihaan.clubmanagement.repository.absences.AbsenceApprovalStatusRepository;
 import com.github.travelervihaan.clubmanagement.repository.absences.AbsenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Service
 public class AbsenceService {
 
     private final String WAITING_STATUS = "WAITING";
+    private final String APPROVED_STATUS = "APPROVED";
+    private final String REJECTED_STATUS = "REJECTED";
 
     private AbsenceRepository absenceRepository;
     private AbsenceApprovalStatusRepository absenceApprovalStatusRepository;
 
+    @Autowired
     public AbsenceService(AbsenceRepository absenceRepository,
                           AbsenceApprovalStatusRepository absenceApprovalStatusRepository){
         this.absenceRepository = absenceRepository;
@@ -26,7 +31,7 @@ public class AbsenceService {
         return absenceRepository.findAll();
     }
 
-    public void addNewAbsence(Absence absence, BindingResult result) {
+    public void addNewAbsence(@Valid Absence absence, BindingResult result) {
         if (!result.hasErrors()) {
             if (isApprovalStatusExist(WAITING_STATUS))
                 absence.setAbsenceApprovalStatus(absenceApprovalStatusRepository.findByStatus(WAITING_STATUS).get());
@@ -34,9 +39,28 @@ public class AbsenceService {
         }
     }
 
+    public void changeAbsenceStatus(long absenceId, String newStatus){
+        if(newStatus.equals(APPROVED_STATUS) || newStatus.equals(REJECTED_STATUS))
+            changeStatus(absenceId, newStatus);
+    }
+
     private boolean isApprovalStatusExist(String status){
         return absenceApprovalStatusRepository.findByStatus(status).isPresent();
     }
 
+    private boolean isAbsenceExist(long absenceId){
+        return absenceRepository
+                .findById(absenceId)
+                .isPresent();
+    }
 
+    private void changeStatus(long absenceId, String status) {
+        if(isAbsenceExist(absenceId)) {
+            Absence absence = absenceRepository
+                    .findById(absenceId)
+                    .get();
+            absence.getAbsenceApprovalStatus().setStatus(status);
+            absenceRepository.save(absence);
+        }
+    }
 }
