256eaa353dc8f5c14c0301288cc8d12b6e25424f
TravelerVihaan <lukasz.biel@10g.pl>
2019-07-15 13:55:56 +0200

    Header

--- src/main/java/com/github/travelervihaan/clubmanagement/controller/LoginController.java
+++ src/main/java/com/github/travelervihaan/clubmanagement/controller/LoginController.java
@@ -1,13 +1,15 @@
 package com.github.travelervihaan.clubmanagement.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
-	public String loginPage() {
+	@GetMapping("/login")
+	public String loginPage(@RequestParam(required = false) boolean error) {
+
 		return "login";
 	}
 }
